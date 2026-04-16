65528e697be3692f0d1a184e99ffb8e2ad0cc055
Adolfo Gante <adolfogante@gmail.com>
2026-04-16 13:37:50 -0700

    links to pages

--- components/DesktopNav.tsx
+++ components/DesktopNav.tsx
@@ -1,27 +1,34 @@
 import { Button } from './ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const mainNavItems = ['Home', 'Bakealongs', 'Recipes'];
+const mainNavItems = [
+    ['Home', '/'],
+    ['Bakealongs', '/bakealongs'],
+    ['Recipes', '/recipes']
+];
 
 export default function DesktopNav() {
     return (
         <nav className="bg-muted">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3">
-                <Image
-                    src="/pepoHappyChef.webp"
-                    alt="website logo"
-                    width={32}
-                    height={32}
-                />
+                <Link href='/'>
+                    <Image
+                        src="/pepoHappyChef.webp"
+                        alt="website logo"
+                        width={32}
+                        height={32}
+                    />
+                </Link>
 
                 <div className="flex items-center gap-6">
-                    {mainNavItems.map((item) => (
+                    {mainNavItems.map(([name, link]) => (
                         <Button
-                            key={item}
+                            key={name}
                             variant="link"
                             className="text-foreground cursor-pointer"
                         >
-                            {item}
+                            <Link href={link}>{name}</Link>
                         </Button>
                     ))}
                 </div>
